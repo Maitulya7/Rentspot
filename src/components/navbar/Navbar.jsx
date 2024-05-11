c0397e7a3c4385a56e95c439f2b0aded7c89b808
maitulya7 <vaghelamaitulya85@gmail.com>
2024-05-11 11:46:26 +0530

    Navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import "./navbar.scss";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="left">
+        <a href="/" className="logo">
+          <img src="/logo.png" alt="logo" />
+          <span>RentSpot</span>
+        </a>
+        <a href="/home">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+        <a href="/agents">Agents</a>
+      </div>
+      <div className="right">
+        <a href="/sign-in">Sing In</a>
+        <a href="/sign-up" className="register">Sign Up</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;

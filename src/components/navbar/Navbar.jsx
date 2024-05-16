9c6f14db65938684cf7a20eb720582f2492742db
maitulya7 <vaghelamaitulya85@gmail.com>
2024-05-16 10:48:19 +0530

    Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import "./navbar.scss";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  console.log(open);
   return (
     <nav>
       <div className="left">
@@ -15,7 +18,26 @@ const Navbar = () => {
       </div>
       <div className="right">
         <a href="/sign-in">Sing In</a>
-        <a href="/sign-up" className="register">Sign Up</a>
+        <a href="/sign-up" className="register">
+          Sign Up
+        </a>
+        <div className={open ? "menuItem active" : "menuItem"}>
+          <img
+            src="./menu.png"
+            alt="menu"
+            onClick={() => setOpen((prev) => !prev)}
+          />
+        </div>
+        <div className={open ? "menu active" : "menu"}>
+          <a href="/home">Home</a>
+          <a href="/about">About</a>
+          <a href="/contact">Contact</a>
+          <a href="/agents">Agents</a>
+          <a href="/sign-in">Sing In</a>
+          <a href="/sign-up" className="register">
+            Sign Up
+          </a>
+        </div>
       </div>
     </nav>
   );

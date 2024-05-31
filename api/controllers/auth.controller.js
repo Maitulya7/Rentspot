6ddf64e0ee15717192bc9e659c07c54348eb1dce
maitulya7 <vaghelamaitulya85@gmail.com>
2024-05-31 10:02:48 +0530

    API

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,15 @@
 import bcrypt from "bcrypt";
 import prisma from "../lib/prisma.js";
+import asyncHandler from "express-async-handler";
 
-const Register = async (req, res) => {
+const Register = asyncHandler(async (req, res) => {
   const { username, email, password } = req.body;
+
+  //all fields are required
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error("All fields are required");
+  }
 
   //Hash password
   const hashPassword = await bcrypt.hash(password, 10);
@@ -16,12 +23,40 @@ const Register = async (req, res) => {
     },
   });
 
-  res.status(201).json({message:"User created successfully", data: newUser});
+  res.status(201).json({ message: "User created successfully", data: newUser });
+});
 
-};
-const Login = (req, res) => {
-  res.send("login");
-};
+const Login = asyncHandler(async (req, res) => {
+  const { username, password } = req.body;
+
+  //all fields are required
+  if (!username || !password) {
+    res.status(400);
+    throw new Error("All fields are required");
+  }
+
+  //check if user exits in the database
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  });
+
+  if (!user) {
+    res.status(401);
+    throw new Error("Invalid credentials");
+  }
+  const isPasswordMatch = await bcrypt.compare(password, user.password);
+  if (!isPasswordMatch) {
+    res.status(401);
+    throw new Error("Invalid credentials");
+  }
+
+ 
+
+
+});
+
 const Logout = (req, res) => {
   res.send("logout");
 };
